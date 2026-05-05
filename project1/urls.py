7163d2daa834548c7038c7b3550883f7d020fc9e
@chowdhury1 <md.samiul.chowdhury@tuhh.de>
2026-05-05 13:50:42 +0200

    conflict merged

--- project1/urls.py
+++ project1/urls.py
@@ -5,13 +5,13 @@ app_name = 'project1'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload/', views.upload_csv, name='upload'), 
-    path('plot/', views.generate_plot, name='plot'), 
-    path('generate-plot/', views.generate_plot_ajax, name='generate_plot_ajax'),
+    # path('upload/', views.upload_csv, name='upload'), 
+    # path('plot/', views.generate_plot, name='plot'), 
+    # path('generate-plot/', views.generate_plot_ajax, name='generate_plot_ajax'),
 
     #urls_for_modeltraining
-    path('mtrain/', views.train_page, name='mtrain'),
-    path('train-model-form/', views.train_model_form_view, name='train_model_form'),
-    path("train/", views.train, name="train"),
+    path('mtrain/', views.train, name='mtrain'),
+#     path('train-model-form/', views.train, name='train_model_form'),
+#     path("train/", views.train, name="train"),
 ]
 
